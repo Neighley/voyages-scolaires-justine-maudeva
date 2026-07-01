00cb2fa8e3d816bbcf08be2e432337be54265bf2
EvaLfrc <maudeva62@gmail.com>
2026-07-01 14:18:23 +0200

    Phase 2 : correction des modèles fillable, relations, routes web, politiques et complétion du CRUD Voyages

--- www/routes/web.php
+++ www/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\VoyageController; 
+use App\Http\Controllers\ParticipantController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,6 +18,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::resource('voyages', VoyageController::class);
+    Route::post('/voyages/{voyage}/participants', [ParticipantController::class, 'store'])->name('voyages.participants.store');
+    Route::patch('/participants/{participant}/autoriser', [ParticipantController::class, 'autoriser'])->name('participants.autoriser');
 });
 
 require __DIR__.'/auth.php';
